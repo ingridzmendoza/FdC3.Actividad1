17aad56711bc71d4ecb950c809934f0d5d74d607
Ingrid m <99279382+ingridzmendoza@users.noreply.github.com>
2023-08-30 09:50:36 -0700

    Add files via upload

---
+++ Act1Ej1/src/act1ej1/Act1Ej1.java
@@ -0,0 +1,18 @@
+/*
+main actividad 1 ejercicio 1
+*/
+package act1ej1;
+
+/**
+ * @vxndett
+ */
+public class Act1Ej1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaColores ventana = new VentanaColores(); //mandar llamar al constructor
+        ventana.setVisible(true);
+        
+    }//end main
+    
+}
