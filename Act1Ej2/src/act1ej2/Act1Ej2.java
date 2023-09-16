038cedfed40c396f85efd1ffe50b325104d12866
Ingrid m <99279382+ingridzmendoza@users.noreply.github.com>
2023-09-15 17:29:34 -0700

    Add files via upload

---
+++ Act1Ej2/src/act1ej2/Act1Ej2.java
@@ -0,0 +1,21 @@
+
+package act1ej2;
+
+/**
+ *main
+ * @vxndett
+ */
+public class Act1Ej2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaAnimales ventana = new VentanaAnimales();
+        ventana.setResizable(false);
+        ventana.setVisible(true);
+        
+    }
+    
+}
