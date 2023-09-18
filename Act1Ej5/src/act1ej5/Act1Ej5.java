5c7072f228d8409f4000592461d1c2e815a034f8
Ingrid m <99279382+ingridzmendoza@users.noreply.github.com>
2023-09-17 21:43:41 -0700

    Add files via upload

---
+++ Act1Ej5/src/act1ej5/Act1Ej5.java
@@ -0,0 +1,17 @@
+
+package act1ej5;
+
+/**
+ *
+ * @author vxndett
+ */
+public class Act1Ej5 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaBotones ventana = new VentanaBotones();
+        ventana.setResizable(false);
+        ventana.setVisible(true);
+    } //end main
+    
+}
